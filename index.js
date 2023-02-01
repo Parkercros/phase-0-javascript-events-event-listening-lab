852d5f40787075d5d6e9b70bd98aeb8cd2fe4292
Parker Crosgrove <pdxparker3@gmail.com>
2023-02-01 13:29:29 -0700

    finished lab

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 function addingEventListener() {
-    const input = document.getElementById("button");
-    input.addEventListener("click", function() {
-      alert("I was clicked!");
-    });
-  }
-  
-  addingEventListener();
-  
+  const input = document.getElementById("button");
+  input.addEventListener("click", function () {
+    alert("I was clicked!");
+  });
+}
